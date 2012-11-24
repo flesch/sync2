bc5aac52606526a64dc5cff9c739e57900c84ce3
John Flesch <john@fles.ch>
2012-11-24 12:12:08 -0600

    Adding front page.

--- app.js
+++ app.js
@@ -26,8 +26,12 @@ app.configure('production', function(){
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/*", function(req, res){
+app.get("/", function(req, res){
   res.sendfile(__dirname + '/public/index.html');
+});
+
+app.get("/*", function(req, res){
+  res.sendfile(__dirname + '/public/app.html');
 });
 
 app.use(app.router);
